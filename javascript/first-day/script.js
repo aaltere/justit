e512857fa3420684874a69361361c5739d9c559c
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-15 11:22:18 +0000

    Add ways to create variables

--- javascript/first-day/script.js
+++ javascript/first-day/script.js
@@ -23,3 +23,28 @@ let num2 = 50;
 console.log(num2);
 
 // There are four ways to create a variable in JS
+
+// Let
+
+let number1 = 10;
+console.log(number1);
+
+number1 = 20;
+console.log(number1);
+
+// Var
+
+var number2 = 10;
+var number2 = 20;
+{
+    var number2 = 30;
+}
+
+console.log(number2);
+
+// Const
+
+const number3 = 10;
+
+// Shows error as shouldn't be re-assign
+// number3 = 20;
