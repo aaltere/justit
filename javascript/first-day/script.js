bd27c004cd6e862b7ffc3634e9b3001dab3bbc7f
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-15 10:32:22 +0000

    Intro to making variables

--- javascript/first-day/script.js
+++ javascript/first-day/script.js
@@ -10,4 +10,16 @@ console.log(10 / 5);
 console.warn("Warning");
 console.error("Error message");
 
-alert("Do you want to continue");
+// alert("Do you want to continue");
+
+let num1; // Variable declaration
+num1 = 10;  // Variable initialization
+
+console.log(num1);
+
+// Declaring and initializing a variable on the same line
+let num2 = 50;
+
+console.log(num2);
+
+// There are four ways to create a variable in JS
