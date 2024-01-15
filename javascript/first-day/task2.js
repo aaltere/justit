64e1db8b1f4c338cd60bfc021969b403c8005bd5
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-15 11:25:43 +0000

    Task completed

---
+++ javascript/first-day/task2.js
@@ -0,0 +1,14 @@
+// Task 1
+let favFood = "Pizza";
+let favColor = "Red";
+let favDrink = "Lemonade"
+
+// Task 2
+console.log(favDrink);
+
+// Task 3
+favFood = "Pasta";
+console.log(favFood);
+
+// Task 4
+let dob = "1999-07-06";
