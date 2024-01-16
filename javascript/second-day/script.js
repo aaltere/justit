f7ae9caec4932cdebd95ef0c3d77845b8a3e8ff4
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-16 10:33:20 +0000

    Add examples for type and casting

---
+++ javascript/second-day/script.js
@@ -0,0 +1,45 @@
+let num1 = 100; // Number
+let firstName = "Zak"; // String
+
+console.log(typeof num1);
+console.log(typeof firstName);
+console.log(typeof 50);
+
+let education = true; // Boolean
+console.log(education);
+
+let a = 10;
+console.log(a == 10);
+
+let b;
+console.log(typeof b);
+
+let c = null;
+console.log(typeof c);
+
+let e = 958.609;
+console.log(typeof e);
+
+let bigInt = 981983542n;
+console.log(typeof bigInt);
+
+console.clear();
+
+let f = 3;
+console.log(typeof String(f));
+
+let g = String(100);
+console.log(typeof g);
+
+let h = String(true);
+console.log(typeof h);
+console.log(h);
+
+let i = "20";
+console.log(Number(i));
+
+let myName = "Peter";
+console.log(Number(myName));
+
+console.log(Number(false));
+console.log(Number(true));
