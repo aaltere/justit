f5a847a8ba947f81dc1abef35ee1e27df286e07e
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-17 11:40:17 +0000

    Task 1 completed

---
+++ javascript/third-day/task1.js
@@ -0,0 +1,14 @@
+let age = 18;
+
+if (age < 0)
+{
+    console.log("Invalid age");
+}
+else if (age < 18)
+{
+    console.log("You are too young to drink");
+}
+else 
+{
+    console.log("What would you like to drink?");
+}
