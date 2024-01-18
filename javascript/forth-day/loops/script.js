167b53b3a1252d3c53b45359dde3e79908c734e1
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-18 10:10:10 +0000

    Add examples for loop

---
+++ javascript/forth-day/loops/script.js
@@ -0,0 +1,22 @@
+// For loop
+for (let i = 0; i < 5; i++)
+{
+    console.log("Welcome to JavaScript");
+}
+
+let colours = [
+    "Blue",
+    "Green",
+    "Red",
+    "Yellow",
+    "Orange",
+    "Black",
+    "Purple",
+    "Pink",
+    "Brown"
+];
+
+for (let i = 0; i < colours.length; i++)
+{
+    console.log(colours[i]);
+}
