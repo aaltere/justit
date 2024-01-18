c5c60504217748679a2011ef303bbd1eceb1da47
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-18 11:40:58 +0000

    Task completed

---
+++ javascript/forth-day/tasks/task1.js
@@ -0,0 +1,12 @@
+const shows = [
+    "Suits", 
+    "Breaking bad", 
+    "V for vendetta", 
+    "Pulp fiction", 
+    "South park"
+];
+
+for (const show in shows)
+{
+    console.log(shows[show]);
+}
