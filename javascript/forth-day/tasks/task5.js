d99251ca7a59e6e26dc0705a82b71ffc9a5301e1
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-18 12:08:54 +0000

    Update task 5

--- javascript/forth-day/tasks/task5.js
+++ javascript/forth-day/tasks/task5.js
@@ -1,9 +1,9 @@
 numbers = [13, 23, 12, 45, 22, 48, 66, 100];
 
-for (const number in numbers)
+for (const number of numbers)
 {
-    if (numbers[number] % 2 === 0)
+    if (number % 2 === 0)
     {
-        console.log(numbers[number]);
+        console.log(number);
     }
 }
