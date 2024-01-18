ad8155dab17c8b4cf599db4db42d67ac7dd69e00
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-18 09:39:46 +0000

    Add examples

---
+++ javascript/forth-day/array/script.js
@@ -0,0 +1,17 @@
+// A single variable which hold more than one value
+let numbers = [2, 4, 6, 7, 10, 11];
+// Each value has a number associated to it called - index number
+// Index number start from zero
+
+let employees = [
+    "Zak",
+    "Mark",
+    "Christian",
+    "Timothy",
+    "Abdul"
+];
+
+console.log(employees[0]);
+console.log(employees);
+
+console.log(employees.length);
