1690572f51be6d107e341240dee3bbc3a891f1a2
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-24 02:17:31 +0000

    Add more ways to get reference

--- javascript/sixth-day/dom/script.js
+++ javascript/sixth-day/dom/script.js
@@ -1,2 +1,8 @@
 const main = document.getElementById("in-one");
 console.log(main);
+
+const test = document.getElementById("test");
+console.log(test.value);
+
+// You can use getElementByClassName and getElementByTagName
+// You can also use querySelector and querySelectorAll
