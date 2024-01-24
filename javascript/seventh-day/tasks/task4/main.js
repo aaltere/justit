c287e1a59f40d71b573d889ebd5904554f87b6ac
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-24 10:04:55 +0000

    Task completed

---
+++ javascript/seventh-day/tasks/task4/main.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("input");
+
+input.addEventListener("keypress", () =>
+{
+    input.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    input.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+});
