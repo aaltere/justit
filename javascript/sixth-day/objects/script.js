10b407a3b40a831e9c83a6a8e3a2a5ce62b96eb5
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-24 01:55:15 +0000

    Add object example

---
+++ javascript/sixth-day/objects/script.js
@@ -0,0 +1,12 @@
+let library = {
+    JamesAndGiantPeach: {
+        author: "", 
+        ISBN: 709257840540, 
+        location: "E789", 
+        grenre: "Children's"
+    }
+}
+
+// {} defines we are using an object
+
+console.log(library.JamesAndGiantPeach.ISBN);
