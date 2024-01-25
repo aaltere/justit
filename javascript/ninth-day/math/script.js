d3a6f9480687d18dd947f5b2fab57e95c46fc8c7
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-25 12:27:07 +0000

    Add maths method and example

---
+++ javascript/ninth-day/math/script.js
@@ -0,0 +1,15 @@
+// PI - returns value of pi
+// SQRTx - returns square root of x
+
+// min() - used to find lowest value in list of arguments
+// max() - used to find highest value in list of arguments
+// round() - round to nearest interger
+// sqrt() - used to return square root of x rounded to nearest integer
+// pow() - takes (x, y) returns x to power of y
+// floor() - round x down to nearest integer
+// ceil() - round x up to nearest integer
+// random() - returns random number between 0 - 1
+// cos() takes x, returns the cosine of an angle in radians, value between -1 and 1
+// sin() takes x, returns the sine of an angle in radians, value between -1 and 1
+
+let num = Math.ceil(Math.random() * 25);
