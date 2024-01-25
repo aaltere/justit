1a6f55e2a2d5b4ff277c24cd7a9770d9f324d021
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-25 12:08:29 +0000

    Add localestring displaying date

--- javascript/ninth-day/date/script.js
+++ javascript/ninth-day/date/script.js
@@ -1,3 +1,16 @@
 const divDate = document.querySelector("#date");
 
 const d = new Date();
+
+const date = d.toLocaleString("default", 
+{
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hourCycle: "h24"
+});
+
+divDate.textContent = date;
