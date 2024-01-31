df33fff97f59586d19de3d1002e655a16573363a
Pete Ashworth <peter0ashworth@hotmail.com>
2024-01-31 10:48:20 +0000

    Draw game window

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -6,3 +6,7 @@ const windowHeight = (canvas.height = window.innerHeight);
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, windowWidth, windowHeight);
+
+ctx.strokeStyle = "cyan";
+ctx.strokeRect((windowWidth / 2) - 125, (windowHeight / 2) - 250, 250, 500);
+
