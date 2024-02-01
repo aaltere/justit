66642805276330221ef79177d907e9efe3f35c1c
Pete Ashworth <peter0ashworth@hotmail.com>
2024-02-01 01:56:24 +0000

    Add comments and x and y blocks amount

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,31 +1,38 @@
+// Get reference to canvas and brush
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+// Get window height and width
 const windowWidth = (canvas.width = window.innerWidth);
 const windowHeight = (canvas.height = window.innerHeight);
 
+// Fill screen black
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, windowWidth, windowHeight);
 
+// Set game window width, height, and the number of blocks per direction
 const gameWidth = 250;
 const gameHeight = 500;
 
+const yBlock = 20;
+const xBlock = 10;
+
+// Get location for where game window starts
 const gameWindowXStart = (windowWidth / 2) - (gameWidth / 2);
 const gameWindowYStart = (windowHeight / 2) - (gameHeight / 2);
 
+// Draw game border
 ctx.strokeStyle = "cyan";
 ctx.strokeRect(gameWindowXStart, gameWindowYStart, gameWidth, gameHeight);
 
 let gameArea = [];
 
-for (let i = 0; i < 20; i++)
+for (let i = 0; i < yBlock; i++)
 {
-    gameArea[i] = new Array(10);
+    gameArea[i] = new Array(xBlock);
 
-    for (let j = 0; j < 10; j++)
+    for (let j = 0; j < xBlock; j++)
     {
         gameArea[i][j] = "O";
     }
 }
-
-console.log(gameArea);
