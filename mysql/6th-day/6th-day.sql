5e3384c8005e4b3476f35882cf008e9509943aeb
Aaltere <53907045+Aaltere@users.noreply.github.com>
2024-02-13 00:02:57 +0000

    Update 6th-day.sql

--- mysql/6th-day/6th-day.sql
+++ mysql/6th-day/6th-day.sql
@@ -61,9 +61,74 @@ SELECT
     name
 FROM
 	employees
-UNION
+UNION ALL
 SELECT
 	id,
     name
 FROM
 	managers;
+    
+USE world;
+
+SELECT
+	countries.name,
+    cities.name
+FROM
+	countries
+		INNER JOIN
+	cities ON countries.code = cities.country_code;
+    
+USE company;
+
+SELECT
+	fname,
+    lname,
+    dname
+FROM
+	employee
+		INNER JOIN
+	department ON dno = dnumber;
+    
+CREATE DATABASE joindb;
+
+USE joindb;
+
+CREATE TABLE employees
+(
+	employee_number INT PRIMARY KEY,
+    employee_name VARCHAR(10),
+    department_number INT
+);
+
+CREATE TABLE departments
+(
+	department_number INT PRIMARY KEY,
+    department_name VARCHAR(30),
+    location VARCHAR(20)
+);
+
+ALTER TABLE employees ADD FOREIGN KEY (department_number)
+	REFERENCES departments(department_number);
+
+INSERT INTO employees(
+	employee_number,
+    employee_name,
+    department_number
+)
+VALUES
+	(1, 'Beautriz', 1),
+    (2, 'Zak', 2),
+    (3, 'Matt', 2),
+    (4, 'Waqas', 2),
+    (5, 'Mark', 3),
+    (6, 'John', NULL);
+    
+INSERT INTO departments(
+	department_number,
+    department_name,
+    location
+)
+VALUES
+	(1, 'HR', 'London'),
+    (2, 'Software', 'London'),
+    (3, 'IT', 'Manchester');
