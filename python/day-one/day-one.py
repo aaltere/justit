faf7bb339da0d99ad79491cef074aa1725ac8055
Pete Ashworth <peter0ashworth@hotmail.com>
2024-02-19 10:35:50 +0000

    Add first day pthon

---
+++ python/day-one/day-one.py
@@ -0,0 +1,42 @@
+print(2 * 2)
+print(3 + 2)
+print(50 / 5)
+print(45 - 7)
+
+print("Peter\nDerby\nProgramming")
+
+testString = "an_object"
+num1 = 10
+num2 = 2.0
+testBool = True
+testBool = False
+
+num1, num2 = 5, 8
+answer = num1 + num2
+print(answer)
+
+print()
+
+paragraph1 = paragraph2 = paragraph3 = "Template paragraph"
+print(paragraph1 + "\n\n" + paragraph2 + "\n\n" + paragraph3)
+
+# chained assignment assign one value to multiple variables
+# assigning multiple variables assing multiple values to their corresponding variables
+
+print()
+
+num1 = 10
+num2 = num1
+
+print(id(num1))
+print(id(num2))
+
+print()
+
+num3 = 10
+num4 = "10"
+
+print(id(num3))
+print(id(num4))
+
+# variable stores data
