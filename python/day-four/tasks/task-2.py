1afee43abe4bf701d0d8164e126e81e9ca5fbbfb
Pete Ashworth <peter0ashworth@hotmail.com>
2024-02-22 11:32:19 +0000

    Task 2 completed

---
+++ python/day-four/tasks/task-2.py
@@ -0,0 +1,7 @@
+def subtraction(num1, num2):
+    return num1 - num2
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print(f"The answer to {num1} - {num2} is {subtraction(num1, num2)}")
