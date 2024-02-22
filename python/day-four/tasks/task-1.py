e4efa818cea28959c8d5f887a40d5709640dee31
Pete Ashworth <peter0ashworth@hotmail.com>
2024-02-22 11:29:03 +0000

    Task 1 completed

---
+++ python/day-four/tasks/task-1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+interest = input("Any interest: ")
+
+def printInfo(name, address, interest):
+    print(f"My name is {name}, my address is {address}, and my interest is {interest}")
+
+printInfo(name, address, interest)
