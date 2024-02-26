5e771419e0dae21eb096e78623c996cf20c1ac1f
Pete Ashworth <peter0ashworth@hotmail.com>
2024-02-26 11:52:57 +0000

    Task completed

--- python/week-10/Pt7_FilesDictsCodeBase2024/1_Reference_File_FileHandling/5_userInput.py
+++ python/week-10/Pt7_FilesDictsCodeBase2024/1_Reference_File_FileHandling/5_userInput.py
@@ -1,12 +1,14 @@
-fname    = input("Enter you full name: ")
-address   = input("Enter your address: ")
+fname = input("Enter you full name: ")
+address = input("Enter your address: ")
 interest = input("Enter your interest: ")
-age      =    int(input("Enter your age: "))
+age = int(input("Enter your age: "))
 
 "Make"
 "To Do: Task 1: Use the code above to ask for user input and then save it to a file called userDetails.txt"
 
-
+with open("python/week-10/Pt7_FilesDictsCodeBase2024/user-details.txt", "a") as filePath:
+    content = f"{fname}\n{address}\n{interest}\n{age}"
+    filePath.write(content)
 
 
 "Further reading"
