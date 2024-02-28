c0251c380ae7a25bb224d63b520613e009b3b90b
Pete Ashworth <53907045+aaltere@users.noreply.github.com>
2024-02-28 02:00:57 +0000

    Add log file

--- python/week-10/Pt8_ExceptionsCodeBase2024/1_Exceptions/1_consoleLogB.py
+++ python/week-10/Pt8_ExceptionsCodeBase2024/1_Exceptions/1_consoleLogB.py
@@ -9,7 +9,7 @@ or set to ERROR to log only CRITICAL and ERROR.
 While if configured to CRITICAL, only critical errors will be logged'''
 
 # logging.basicConfig(filename = "myFilelog.log", level = logging.ERROR)  # from error and above will be logged to file
-logging.basicConfig(filename = r"folderpath/filename", level = logging.DEBUG) # to see all error logs
+logging.basicConfig(filename = r"python\week-10\Pt8_ExceptionsCodeBase2024\1_Exceptions/file1.log", level = logging.DEBUG) # to see all error logs
 #different logging methods and severity
 logging.critical("Critical")
 logging.error("Error")
