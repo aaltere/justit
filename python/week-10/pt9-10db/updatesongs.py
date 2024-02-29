442a9a4b5aeda04b9ed18cbad62438085627ae11
Pete Ashworth <53907045+aaltere@users.noreply.github.com>
2024-02-29 09:42:32 +0000

    Add update song

---
+++ python/week-10/pt9-10db/updatesongs.py
@@ -0,0 +1,33 @@
+from connect import *
+
+def updateSong():
+    
+    try:
+
+        songID = int(input("Enter the SongID of the record to be updated: "))
+        dbCursor.execute(f"SELECT * FROM songs WHERE SongID = {songID}")
+
+        row = dbCursor.fetchone()
+
+        if row == None:
+            print(f"No record with the SongID of {songID}")
+        else:
+            title = input("Enter song title: ")
+            artist = input("Enter song artist: ")
+            genre = input("Enter song genre: ")
+
+            dbCursor.execute("UPDATE songs SET Title = ?, Artist = ?, Genre = ? WHERE SongID = ?", (title, artist, genre))
+            dbConnection.commit()
+            
+            print(f"Record with SongID {songID} updated")
+
+    except sql.OperationalError as e:
+        print(f"Operational Failed: {e}")
+
+    except sql.ProgrammingError as pe:
+        print(f"Programming failed: {pe}")
+
+    except sql.Error as er:
+        print(f"Error failed: {er}")
+
+updateSong()
