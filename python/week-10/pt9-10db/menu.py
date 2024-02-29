a1ad20d227a61ca74ec7f9ac51e287f66b4e4106
Pete Ashworth <53907045+aaltere@users.noreply.github.com>
2024-02-29 12:22:12 +0000

    Add menu for program

---
+++ python/week-10/pt9-10db/menu.py
@@ -0,0 +1,54 @@
+import readsongs, addsongs, updatesongs, deletesongs, reports
+
+def readFile(filePath):
+
+    try:
+        with open(filePath) as readFile:
+            rf = readFile.read()
+            return rf
+        
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+
+def songsMenu():
+    option = 0
+    optionList = ["1", "2", "3", "4", "5", "6"]
+
+    menuChoices = readFile("python/week-10/pt9-10db/menuOptions.txt")
+
+    while option not in optionList:
+        
+        print(menuChoices)
+
+        option = input("Enter an option from the menu choices above: ")
+
+        if option not in optionList:
+            print(f"{option} is not valid")
+
+    return option
+
+mainProgram = True
+
+while mainProgram:
+
+    menuOption = songsMenu()
+
+    match menuOption:
+
+        case "1":
+            readsongs.readSongs()
+            break
+        case "2":
+            addsongs.insertSongs()
+            break
+        case "3":
+            updatesongs.updateSong()
+            break
+        case "4":
+            deletesongs.deleteSong()
+            break
+        case "5":
+            reports.searchSong()
+            break
+        case _:
+            mainProgram = False
