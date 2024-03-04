ddd8ac4d30ae649d0efa2ec24f056489db1aebea
Pete Ashworth <53907045+aaltere@users.noreply.github.com>
2024-03-04 11:19:46 +0000

    Correct names

--- python/project/create-table.py
+++ python/project/create-table.py
@@ -3,12 +3,12 @@ from connect import *
 
 # create table to hold contacts
 dbCursor.execute("""
-CREATE TABLE "songs" (
+CREATE TABLE "contacts" (
 	"contact_id" INTEGER NOT NULL UNIQUE,
 	"full_name"	TEXT,
     "phone" TEXT,
 	"email"	TEXT,
 	"address" TEXT,
     "note" TEXT,
-	PRIMARY KEY("SongID" AUTOINCREMENT)
+	PRIMARY KEY("contact_id" AUTOINCREMENT)
 )""")
