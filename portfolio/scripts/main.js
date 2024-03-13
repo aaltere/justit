f8af6d752aece2fb0f878bd405063699c5b0df75
Pete Ashworth <53907045+aaltere@users.noreply.github.com>
2024-03-13 15:23:15 +0000

    Add transparency to navbar

---
+++ portfolio/scripts/main.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector("header");
+
+window.addEventListener("scroll", () =>
+{
+    if (window.scrollY > 700)
+    {
+        navbar.style.backgroundColor = "rgba(29, 30, 44, 0.5)";
+    }
+    else if (window.scrollY === 0)
+    {
+        navbar.style.backgroundColor = "rgba(29, 30, 44, 1)";
+    }
+});
